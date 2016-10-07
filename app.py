29bbc93ce74af4a8f587e51f0bfa5646f70a4b28
Ken <ng.ken84@gmail.com>
2016-10-06 23:39:44 -0700

    FIX : Fix sessionmaker invalid import

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, User, Category, CategorySubItem
+
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 @app.route('/')
 @app.route('/hello')
 def HelloWorld():
-	return "Hello World"
+	return render_template('index.html')
+
 
 if __name__ == '__main__':
 	app.debug = True

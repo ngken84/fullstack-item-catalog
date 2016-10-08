b319ca0d0eabf624f565fd4e9f7ff8c0c81aa097
Ken <ng.ken84@gmail.com>
2016-10-08 14:35:43 -0700

    FEAT : Add google sign in button. Callback still needs to be coded

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
+from flask import session as login_session
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, User, Category, CategorySubItem
+import random
+import string
+
 
 engine = create_engine('sqlite:///catalog.db')
 Base.metadata.bind = engine
@@ -11,10 +15,18 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+def generate_forgery_token():
+	""" generates and returns a 32 character string of letters and numbers to
+	be used as an anti forgery token"""
+	return ''.join(random.choice(string.ascii_uppercase + string.digits)
+		for x in range(32))
+
+
 @app.route('/')
-@app.route('/hello')
-def HelloWorld():
-	return render_template('index.html')
+def MainPage():
+	token = generate_forgery_token()
+	is_logged_in = 'credentials' in login_session
+	return render_template('index.html', logged_in=is_logged_in)
 
 
 if __name__ == '__main__':

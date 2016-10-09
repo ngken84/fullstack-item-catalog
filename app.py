bba4d15eeb452d28e12496b71929a9b5f9c27a40
Ken <ng.ken84@gmail.com>
2016-10-09 08:30:04 -0700

    FEAT : Add google plus sign in callback function /gconnect

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response, request
 from flask import session as login_session
 app = Flask(__name__)
 
@@ -7,13 +7,21 @@ from sqlalchemy.orm import sessionmaker
 from database_setup import Base, User, Category, CategorySubItem
 import random
 import string
+import json
+import httplib2
+import requests
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
 
+CLIENT_ID = json.loads(
+	open('client_secrets.json', 'r').read())['web']['client_id']
 
 engine = create_engine('sqlite:///catalog.db')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
+
 
 def generate_forgery_token():
 	""" generates and returns a 32 character string of letters and numbers to
@@ -25,9 +33,81 @@ def generate_forgery_token():
 @app.route('/')
 def MainPage():
 	token = generate_forgery_token()
+	login_session['state'] = token
 	is_logged_in = 'credentials' in login_session
-	return render_template('index.html', logged_in=is_logged_in)
+	return render_template('index.html',
+							logged_in=is_logged_in,
+							client_id=CLIENT_ID,
+							forgery_token=token)
 
+@app.route('/gconnect', methods=['POST'])
+def GoogleConnect():
+	print('google connect')
+	if request.args.get('state') != login_session['state']:
+		response = make_response(json.dumps('Invalid state'), 401)
+		response.headers['Content-Type'] = 'application/json'
+		print('0')
+		return response
+	code = request.data
+	try:
+		oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+		oauth_flow.redirect_uri = 'postmessage'
+		credentials = oauth_flow.step2_exchange(code)
+	except FlowExchangeError:
+		response = make_response(json.dumps('Failed to upgrade the authorization code'), 401)
+		response.headers['Content-Type'] = 'application/json'
+		print('1')
+		return response
+	access_token = credentials.access_token
+	url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token)
+	h = httplib2.Http()
+	result = json.loads(h.request(url, 'GET')[1])
+	if result.get('error') is not None:
+		response = make_response(json.dumps(result.get('error')), 50)
+		response.headers['Content-Type'] = 'application/json'
+		print('2')
+		return response
+	gplus_id = credentials.id_token['sub']
+	if result['user_id'] != gplus_id:
+		response = make_response(json.dumps("Token's user ID doesn't match given user ID."), 401)
+		response.headers['Content-Type'] = 'application/json'
+		print('3')
+		return response
+	if result['issued_to'] != CLIENT_ID:
+		response = make_response(json.dumps("Token's client ID does not match app!"), 401)
+		response.headers['Content-Type'] = 'application/json'
+		print('4')
+		return response
+	stored_credentials = login_session.get('credentials')
+	stored_gplus_id = login_session.get('gplus_id')
+	if stored_credentials is not None and gplus_id == stored_gplus_id:
+		response = make_response(json.dumps('Current user is already connected.'), 200)
+		response.headers['Content-Type'] = 'application/json'
+		print('5')
+		return response
+	login_session['credentials'] = credentials.access_token
+	login_session['gplus_id'] = gplus_id
+
+	userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+	params = {'access_token': credentials.access_token, 'alt': 'json'}
+	answer = requests.get(userinfo_url, params=params)
+	data = json.loads(answer.text)
+
+	login_session['username'] = data["name"]
+	login_session['picture'] = data["picture"]
+	print(login_session['picture'])
+	login_session['email'] = data["email"]
+
+	response = make_response(json.dumps('Success'), 200)
+	response.headers['Content-Type'] = 'application/json'
+	return response
+
+
+
+
+
+
+app.secret_key = "A980KJSasdkc9834KAXI9dfm32198D98cs8MDF0"
 
 if __name__ == '__main__':
 	app.debug = True

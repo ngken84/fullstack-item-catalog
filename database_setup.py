9f18e95d89fb2fe475dd1071966e96a273dcffd8
Ken <ng.ken84@gmail.com>
2016-10-09 17:31:34 -0700

    FIX : fix setup for database setup

--- database_setup.py
+++ database_setup.py
@@ -1,15 +1,11 @@
 import sys
 
-from sqlalchemy import Column, ForeignKey, Integer, String, Text
+from sqlalchemy import Table, Column, ForeignKey, Integer, String, Text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
 Base = declarative_base()
-
-engine = create_engine('sqlite:///catalog.db')
-
-Base.metadata.create_all(engine)
 
 class User(Base):
 	__tablename__ = 'user'
@@ -17,17 +13,18 @@ class User(Base):
 	email = Column(String(100), nullable=False)
 	service = Column(String(20), nullable=False)
 
-
 class Category(Base):
 	__tablename__ = 'category'
 	id = Column(Integer, primary_key=True)
 	name = Column(String(40), nullable=False)
 	description = Column(Text)
 
-
 class CategorySubItem(Base):
 	__tablename__ = 'category_sub_item'
 	id = Column(Integer, primary_key=True)
 	name = Column(String(40), nullable=False)
 	description = Column(Text)
-	category = relationship(Category)
+	category_id = Column(Integer, ForeignKey('category.id'))
+
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.create_all(engine)
